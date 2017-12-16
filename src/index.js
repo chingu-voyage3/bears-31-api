3fa808a3c1d2f0d1c26a354847f1122afb8ef992
Oxyrus <im.oxyrus@gmail.com>
2017-12-16 15:57:13 -0500

    Move file and load custom router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const router = require("./router");
 
 const app = express();
 
@@ -13,7 +14,9 @@ app.get("/", (req, res) => {
   res.json("Hello world!");
 });
 
-const PORT = process.env.PORT || 6000;
+app.use("/api", router);
+
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`There will be dragons on port ${PORT}`);
