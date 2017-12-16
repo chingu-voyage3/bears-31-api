e22bc48dd78e4f8eff30bed593f86b14c227233a
DagmarTimmreck <DagmarTimmreck@users.noreply.github.com>
2017-12-16 22:14:04 +0100

    export app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
-const app = express();
+export const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
