517d44f6172bb9c596ed028fc6f72b6b0386be9d
Oxyrus <im.oxyrus@gmail.com>
2017-12-28 20:48:17 -0500

    Write functionality for registration/login of users

--- src/router/users.js
+++ src/router/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const models = require('../models');
 const jwtUtils = require('../utils/jwt');
 
 const router = express.Router();
@@ -9,11 +11,20 @@ const router = express.Router();
  * @param {object} res - The response object to write to
  * @return {object} The just created user
  */
-router.post('/users/register', (req, res) => {
-  let response = {
-    endpoint: 'Register a new user'
-  };
-  res.json(response);
+router.post('/users/register', async (req, res) => {
+  const { username, firstName, lastName, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  models.User.create({
+    username,
+    email,
+    first_name: firstName,
+    last_name: lastName,
+    password: hashedPassword,
+  }).then((user) => {
+    res.json(user);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 /**
@@ -22,16 +33,33 @@ router.post('/users/register', (req, res) => {
  * @param {object} res - The response object to write to
  * @return {object} The JWT
  */
-router.post('/users/login', (req, res) => {
-  const obj = {
-    username: req.body.username
-  };
-  const token = jwtUtils.encodeToken(obj);
-  let response = {
-    endpoint: 'Get a JWT to login',
-    token
-  };
-  res.json(response);
+router.post('/users/login', async (req, res) => {
+  const { username, password } = req.body;
+  models.User.findOne({
+    where: {
+      username,
+    },
+  }).then((user) => {
+    bcrypt.compare(password, user.password, (err, valid) => {
+      if (valid) {
+        const payload = {
+          username: user.username,
+        };
+        const token = jwtUtils.encodeToken(payload);
+        const response = {
+          token,
+        };
+        res.json(response);
+      } else {
+        const response = {
+          error: 'Invalid credentials',
+        };
+        res.json(response);
+      }
+    });
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
