c40a23884aa1feeef6ded9fe257d2d8b8060e5e8
Oxyrus <im.oxyrus@gmail.com>
2017-12-31 11:05:53 -0500

    More fixes for production

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,14 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config.js')[env];
-var db        = {};
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config.js')[env];
+let db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
