8b65ec9c8cfa855c09e4de074f35cfb3f479fd91
Oxyrus <im.oxyrus@gmail.com>
2018-01-02 11:59:55 -0500

    Comment group and meeting endpoints meanwhile

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const groupsRouter = require('./groups');
-const meetingsRouter = require('./meetings');
+// const groupsRouter = require('./groups');
+// const meetingsRouter = require('./meetings');
 const usersRouter = require('./users');
 
 const router = express.Router();
 
-router.use(groupsRouter);
-router.use(meetingsRouter);
+// router.use(groupsRouter);
+// router.use(meetingsRouter);
 router.use(usersRouter);
 
 module.exports = router;
