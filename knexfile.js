b66346a4a0920b26585339d06c142ceaa7ad3dbc
Oxyrus <im.oxyrus@gmail.com>
2018-01-02 11:58:34 -0500

    Create config file for knex

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'pg',
+  // connection: process.env.DATABASE_URL,
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'hemsut_api',
+    charset: 'utf8',
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+};
