30746d97a2fb4ef1aa53df69f995268c60b26f59
Oxyrus <im.oxyrus@gmail.com>
2018-01-02 16:55:21 -0500

    Remove old model

--- src/models/group.js
+++ src/models/group.js
@@ -23,37 +23,3 @@ const Groups = bookshelf.Collection.extend({
 
 module.exports.Group = bookshelf.model('Group', Group);
 module.exports.Groups = bookshelf.model('Groups', Groups);
-
-/*
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Group = sequelize.define(
-    'Group',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      underscored: true,
-      classMethods: {
-        associate(models) {
-          Group.belongsToMany(models.User, { through: models.UserGroup });
-          Group.hasMany(models.UserGroup);
-          Group.hasMany(models.Invitation);
-          Group.hasMany(models.Meeting);
-        },
-      },
-    },
-  );
-  return Group;
-};
-*/
