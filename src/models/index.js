39ca4aeb80eb873229efda7e208c26e56b743ab2
Oxyrus <im.oxyrus@gmail.com>
2018-01-02 12:10:01 -0500

    Add models index

--- src/models/index.js
+++ src/models/index.js
@@ -1,35 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const { User, Users } = require('./user');
 
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config.js')[env];
-let db = {};
-
-if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL, config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = {
+  User,
+  Users,
+};
